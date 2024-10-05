c2d3a2d388f8ee038e0078e9f8c1789060048a32
WillianDDaniel <williandeivitidaniel@live.com>
2024-10-05 14:54:55 -0300

    feat: created table car_brand and added registers (task 04)

---
+++ create_carbrand_table.sql
@@ -0,0 +1,16 @@
+-- SQLite (TAKS 04)
+CREATE TABLE CAR_BRAND (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    BRAND_NAME VARCHAR(32) NOT NULL
+);
+
+INSERT INTO CAR_BRAND (BRAND_NAME)
+VALUES
+('Chevrolet'),
+('Toyota'),
+('Hyundai'),
+('Volkswagen'),
+('Jeep'),
+('Renault'),
+('Honda'),
+('Fiat');
